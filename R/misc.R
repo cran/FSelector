04f271f14fba8652b1ba48e065b58dbbc8b36bbd
Piotr Romanski <p.romanski@stud.elka.pw.edu.pl>
2009-04-11 00:00:00 +0000

    version 0.15

---
+++ R/misc.R
@@ -0,0 +1,12 @@
+as.simple.formula <- function(attributes, class) {
+	return(as.formula(paste(class, paste(attributes, sep = "", collapse = " + "), sep = " ~ ")))
+}
+
+get.data.frame.from.formula <- function(formula, data) {
+	d = model.frame(formula, data, na.action = NULL)
+	for(i in 1:dim(d)[2]) {
+		if(is.logical(d[[i]]))
+			d[[i]] = factor(d[[i]])
+	}
+	return(d)
+}
